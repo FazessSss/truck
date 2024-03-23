fcaf09d307fef712ba87abeb1e7edfb19c59e1e1
FazessSss <fazetase0@gmail.com>
2024-03-23 13:30:04 +0300

    dsf
    ds

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-var swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
-  direction: getDirection(),
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  on: {
-    resize: function () {
-      swiper.changeDirection(getDirection());
-    },
-  },
-});
-
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-  return direction;
-}
+    var swiper = new Swiper(".truck", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      loop: true,
+      /* autoplay:{
+        delay:2500, 
+      } */
+    });
