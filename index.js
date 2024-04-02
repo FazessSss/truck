6a05011c92ce28e82d32f991ab850a79f4e38ace
FazessSss <fazetase0@gmail.com>
2024-04-02 20:15:34 +0300

    цау
    цуа

--- index.js
+++ index.js
@@ -6,7 +6,19 @@
         prevEl: ".button-prev",
       },
       loop: true,
-      /* autoplay:{
+       autoplay:{
+        delay:2500, 
+      } 
+    });
+    var swiper = new Swiper(".car", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+      },
+      /*loop: true,
+       autoplay:{
         delay:2500, 
       } */
     });
